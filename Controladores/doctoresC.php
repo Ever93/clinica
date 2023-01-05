c9ee5629a4929abe5455a7ffe3234dd5bc08713d
Ever <informaticoebz@gmail.com>
2023-01-05 17:20:59 -0300

    Actualizar perfil doctor

--- Controladores/doctoresC.php
+++ Controladores/doctoresC.php
@@ -145,4 +145,212 @@ class DoctoresC{
 		}
 	}
 
+
+
+	//Ver perfil Doctor
+	public function VerPerfilDoctorC(){
+
+		$tablaBD = "doctores";
+
+		$id = $_SESSION["id"];
+
+		$resultado = DoctoresM::VerPerfilDoctorM($tablaBD, $id);
+
+		echo '<tr>
+				<td>'.$resultado["usuario"].'</td>
+				<td>'.$resultado["clave"].'</td>
+				<td>'.$resultado["nombre"].'</td>
+				<td>'.$resultado["apellido"].'</td>';
+
+				if($resultado["foto"] == ""){
+
+					echo '<td><img src="Vistas/img/defecto.png" width="40px"></td>';
+
+				}else{
+
+					echo '<td><img src="'.$resultado["foto"].'" width="40px"></td>';
+				}
+
+				$columna= "id";
+				$valor = $resultado["id_consultorio"];
+
+				$consultorio = ConsultoriosC::VerConsultoriosC($columna, $valor);
+
+				echo '<td>'.$consultorio[0]["nombre"].'</td>';
+				
+
+				echo '<td>
+					Desde: '.$resultado["horarioE"].'
+					<br>
+					Hasta: '.$resultado["horarioS"].'
+				</td>
+
+				<td>
+					<a href="http://localhost/clinica/perfil-D/'.$resultado["id"].'">
+						<button class="btn btn-success"><i class="fa fa-pencil"></i></button>
+					</a>
+
+				</td>
+
+			</tr>';
+
+	}
+
+
+	//Editar Perfil Doctor
+	public function EditarPerfilDoctorC(){
+
+		$tablaBD = "doctores";
+		$id = $_SESSION["id"];
+
+		$resultado = DoctoresM::VerPerfilDoctorM($tablaBD, $id);
+
+		echo '<form method="post" enctype="multipart/form-data">
+					
+					<div class="row">
+						
+						<div class="col-md-6 col-xs-12">
+							
+							<h2>Nombre:</h2>
+							<input type="text" class="input-lg" name="nombrePerfil" value="'.$resultado["nombre"].'">
+							<input type="hidden" name="Did" value="'.$resultado["id"].'">	
+
+							<h2>Apellido:</h2>
+							<input type="text" class="input-lg" name="apellidoPerfil" value="'.$resultado["apellido"].'">
+
+							<h2>Usuario:</h2>
+							<input type="text" class="input-lg" name="usuarioPerfil" value="'.$resultado["usuario"].'">
+
+							<h2>Contraseña:</h2>
+							<input type="text" class="input-lg" name="clavePerfil" value="'.$resultado["clave"].'">';
+
+
+				$columna = "id";
+				$valor = $resultado["id_consultorio"];
+
+				$consultorio = ConsultoriosC::VerConsultoriosC($columna, $valor);
+
+				echo '<h2>Consultorio Actual: '.$consultorio[0]["nombre"].'</h2>
+					<h3>Cambiar Consultorio</h3>
+							<select class="input-lg" name="consultorioPerfil">';
+								
+							$columna = null;
+							$valor = null;
+
+							$consultorio = ConsultoriosC::VerConsultoriosC($columna, $valor);
+
+							foreach ($consultorio as $key => $value) {
+								
+								echo '<option value="'.$value["id"].'">'.$value["nombre"].'</option>';
+
+							}
+
+							echo '</select>
+
+							<div class="form-group">
+								
+								<h2>Horario:</h2>
+								Desde: <input type="time" class="input-lg" name="hePerfil" value="'.$resultado["horarioE"].'">
+								Hasta: <input type="time" class="input-lg" name="hsPerfil" value="'.$resultado["horarioS"].'">
+
+							</div>
+
+						</div>
+
+
+						<div class="col-md-6 col-xs-12">
+							
+							<br><br>
+
+							<input type="file" name="imgPerfil">
+							<br>';
+
+							if($resultado["foto"] == ""){
+
+								echo '<img src="http://localhost/clinica/Vistas/img/defecto.png" class="img-responsive" width="200px">';
+
+							}else{
+
+								echo '<img src="http://localhost/clinica/'.$resultado["foto"].'" class="img-responsive" width="200px">';
+
+								
+							}
+							
+
+							echo '<input type="hidden" name="imgActual" value="'.$resultado["foto"].'">
+
+							<br><br>
+
+							<button type="submit" class="btn btn-success">Guardar Cambios</button>
+
+						</div>
+
+					</div>
+
+				</form>';
+
+	}
+
+
+	//Actualizar Perfil Doctor
+	public function ActualizarPerfilDoctorC(){
+
+		if(isset($_POST["Did"])){
+
+			$rutaImg = $_POST["imgActual"];
+
+			if(isset($_FILES["imgPerfil"]["tmp_name"]) && !empty($_FILES["imgPerfil"]["tmp_name"])){
+
+				if(!empty($_POST["imgActual"])){
+
+					unlink($_POST["imgActual"]);
+
+				}
+
+
+				if($_FILES["imgPerfil"]["type"] == "image/png"){
+
+					$nombre = mt_rand(100,999);
+
+					$rutaImg = "Vistas/img/Doctores/Doc-".$nombre.".png";
+
+					$foto = imagecreatefrompng($_FILES["imgPerfil"]["tmp_name"]);
+
+					imagepng($foto, $rutaImg);
+
+				}
+
+				if($_FILES["imgPerfil"]["type"] == "image/jpeg"){
+
+					$nombre = mt_rand(100,999);
+
+					$rutaImg = "Vistas/img/Doctores/Doc-".$nombre.".jpg";
+
+					$foto = imagecreatefromjpeg($_FILES["imgPerfil"]["tmp_name"]);
+
+					imagejpeg($foto, $rutaImg);
+
+				}
+
+			}
+
+			$tablaBD = "doctores";
+
+			$datosC = array("id"=>$_POST["Did"], "nombre"=>$_POST["nombrePerfil"], "apellido"=>$_POST["apellidoPerfil"], "usuario"=>$_POST["usuarioPerfil"], "clave"=>$_POST["clavePerfil"], "consultorio"=>$_POST["consultorioPerfil"], "horarioE"=>$_POST["hePerfil"], "horarioS"=>$_POST["hsPerfil"], "foto"=>$rutaImg);
+
+			$resultado = DoctoresM::ActualizarPerfilDoctorM($tablaBD, $datosC);
+
+			if($resultado == true){
+
+				echo '<script>
+
+				window.location = "http://localhost/clinica/perfil-D/'.$resultado["id"].'";
+				</script>';
+
+			}
+
+		}
+
+	}
+
 }
