357c5b6050d09dc972bd187e44c20508298434c3
Ever <informaticoebz@gmail.com>
2023-01-07 07:55:08 -0300

    Solucion a errores de inicio

--- Controladores/inicioC.php
+++ Controladores/inicioC.php
@@ -11,37 +11,40 @@ class InicioC{
         $resultado = InicioM::MostrarInicioM($tablaBD, $id);
 
         echo '<div class="box-body">
-          
-                <div class="col-md-6 bg-primary" style="margin-top: 5%">
-                <h1>Bienvenidos</h1>
+         
+	          <div class="col-md-6 bg-primary" style="margin-top: 5%">
+	            
+	            <h1>Bienvenidos</h1>
 
-                <h3>'.$resultado["intro"].'</h3>
+	            <h3>'.$resultado["intro"].'</h3>
 
-                <hr>
+	            <hr>
 
-                <h2>Horario:</h2>
-                <h3>Desde: '.$resultado["horarioE"].'</h3>
-                <h3>Hasta: '.$resultado["horarioS"].'</h3>
+	            <h2>Horario:</h2>
+	            <h3>Desde: '.$resultado["horaE"].'</h3>
+	            <h3>Hasta: '.$resultado["horaS"].'</h3>
 
-                <hr>
+	            <hr>
 
-                <h2>Direccion:</h2>
-                <h3>'.$resultado["direccion"].'</h3>
+	            <h2>Dirección:</h2>
+	            <h3>'.$resultado["direccion"].'</h3>
 
-                <hr>
+	            <hr>
 
-                <h2>Contacto:</h2>
-                <h3>Telefono: '.$resultado["telefono"].'<br>
-                Correo: '.$resultado["correo"].'</h3>
+	            <h2>Contactos:</h2>
+	            <h3>Teléfono: '.$resultado["telefono"].' <br>
+	            Correo: '.$resultado["correo"].'</h3>
 
-                </div>
+	          </div>
 
-                <div class="col-md-6">
-                <<img src="Vistas/img/logo.png" class="img-responsive">
-                </div>
+	          <div class="col-md-6">
+	            
+	            <img src="'.$resultado["logo"].'" class="img-responsive">
 
-            </div>';
+	          </div>
 
-    }
+	        </div>';
+
+	}
 
 }
